bd63e30ccad3eb51ea05197ab27fad8233413908
RiinKal <riinkaljurand@gmail.com>
2024-04-11 12:44:31 +0100

    update to gem class

--- sharedMobilityAdventure/src/sharedMobilityAdventure/Gem.java
+++ sharedMobilityAdventure/src/sharedMobilityAdventure/Gem.java
@@ -4,8 +4,6 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-// import java.util.Random;
-
 import javax.imageio.ImageIO;
 
 public class Gem extends Collectable {
