64312c23d454fc59213ca2c5f7a5d9bb5ba378c8
Ryan Davey <104937477+RyanDaveyIE@users.noreply.github.com>
2024-04-23 00:27:16 +0100

    Update Player.java

--- sharedMobilityAdventure/src/sharedMobilityAdventure/Player.java
+++ sharedMobilityAdventure/src/sharedMobilityAdventure/Player.java
@@ -29,7 +29,7 @@ public class Player implements Serializable {
 
     // Image cache for storing loaded images
     @SuppressWarnings("unused")
-	private static transient Map<String, BufferedImage> imageCache = new HashMap<>();
+	private transient static Map<String, BufferedImage> imageCache = new HashMap<>();
 
 
     public Player(GamePanel gamePanel) {
@@ -41,7 +41,7 @@ public class Player implements Serializable {
         this.height = 16;
         this.speed = Main.TILE_SIZE;
         this.tile = Main.TILE_SIZE;
-        this.coins = 100;
+        this.coins = 1000;
         loadImage();
     }
     
@@ -67,7 +67,7 @@ public class Player implements Serializable {
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
                 gamePanel.popupIntersection();
-                gamePanel.timer(10);
+                gamePanel.timer(50);
         	}
         }
         
@@ -81,8 +81,8 @@ public class Player implements Serializable {
                 }
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
-                gamePanel.popupIntersection();
-                gamePanel.timer(10);
+		gamePanel.popupIntersection();
+                gamePanel.timer(50);
         	}
         }
         
@@ -97,7 +97,7 @@ public class Player implements Serializable {
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
                 gamePanel.popupIntersection();
-                gamePanel.timer(10);
+                gamePanel.timer(50);
         	}
         }
         
@@ -112,7 +112,7 @@ public class Player implements Serializable {
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
                 gamePanel.popupIntersection();
-                gamePanel.timer(10);
+                gamePanel.timer(50);
         	}
         }
         
@@ -121,8 +121,8 @@ public class Player implements Serializable {
         	if (taken) {
         		gamePanel.checkGemScore();
         		gamePanel.checkCoinScore();
-                gamePanel.popupIntersection();
-        		gamePanel.timer(10); //CHANGE
+        		gamePanel.popupIntersection();
+        		gamePanel.timer(50); 
         	}
         }
         
@@ -131,8 +131,8 @@ public class Player implements Serializable {
         	if (taken) {
         		gamePanel.checkGemScore();
         		gamePanel.checkCoinScore();
-                gamePanel.popupIntersection();
-        		gamePanel.timer(10); //CHANGE
+        		gamePanel.popupIntersection();
+        		gamePanel.timer(50); 
         	}
         }
     }
@@ -183,35 +183,5 @@ public class Player implements Serializable {
         g.drawImage(image, adjustedX, adjustedY, width, height, null);
     }
   
-//    public int getTimer() {  	
-//    	return playerTime;
-//    }
-//    
-//    public void setTimer(int time) {
-//    	playerTime = time;
-//    	System.out.println("Time updated to: " + playerTime);
-//    }
-//    
-//    public void adjustTimer(double adjustment) {
-//        this.playerTime += adjustment;
-//        System.out.println("Time updated to: " + playerTime);
-//    }
-    
-    public void updateTravel(Route route) {
-        TransportTypes type = route.getTransportType();
-        int distance = route.getTiles().length; // Calculate the number of tiles in the route
-
-        // Carbon Cost
-        double carbonCost = type.calculateCarbonFootprint(distance);
-        adjustCoins(-carbonCost); // Adjust carbon coins based on the cost
-
-        // Time Cost
-        double timePerTile = type.getSpeed(); // Get adjusted speed considering congestion
-        int totalTravelTime = (int) (timePerTile * distance); // Total time taken on route
-        //adjustTimer(-totalTravelTime); // Subtract this time from the game timer   ->>>> FIXXXX
-
-        System.out.println("Travel Costs - Distance: " + distance + ", Carbon Cost: " + carbonCost + ", Time Cost: " + totalTravelTime + " seconds");
-    }
-
 
 }
