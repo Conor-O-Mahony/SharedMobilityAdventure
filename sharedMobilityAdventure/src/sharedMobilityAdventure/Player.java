0606430700cdcff22fe62ba54ca01c91916ba74b
Ryan Davey <104937477+RyanDaveyIE@users.noreply.github.com>
2024-04-23 01:28:37 +0100

    Update Player.java
    Implement Cal 4 changes

--- sharedMobilityAdventure/src/sharedMobilityAdventure/Player.java
+++ sharedMobilityAdventure/src/sharedMobilityAdventure/Player.java
@@ -64,10 +64,13 @@ public class Player implements Serializable {
                 } catch (IOException e1) {
                     e1.printStackTrace();
                 }
+		gamePanel.timer(50);
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
                 gamePanel.popupIntersection();
-                gamePanel.timer(50);
+               if(gamePanel.allGemsCollected() == true) {
+                	gamePanel.restartGame();
+                };
         	}
         }
         
@@ -79,10 +82,13 @@ public class Player implements Serializable {
                 } catch (IOException e2) {
                     e2.printStackTrace();
                 }
+                gamePanel.timer(50);
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
-		gamePanel.popupIntersection();
-                gamePanel.timer(50);
+                gamePanel.popupIntersection();
+               if(gamePanel.allGemsCollected() == true) {
+                	gamePanel.restartGame();
+                };
         	}
         }
         
@@ -94,10 +100,13 @@ public class Player implements Serializable {
                 } catch (IOException e3) {
                     e3.printStackTrace();
                 }
+                gamePanel.timer(50);
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
                 gamePanel.popupIntersection();
-                gamePanel.timer(50);
+               if(gamePanel.allGemsCollected() == true) {
+                	gamePanel.restartGame();
+                };
         	}
         }
         
@@ -109,30 +118,39 @@ public class Player implements Serializable {
                 } catch (IOException e4) {
                     e4.printStackTrace();
                 }
+                gamePanel.timer(50);
                 gamePanel.checkGemScore();
                 gamePanel.checkCoinScore();
                 gamePanel.popupIntersection();
-                gamePanel.timer(50);
+               if(gamePanel.allGemsCollected() == true) {
+                	gamePanel.restartGame();
+                };
         	}
         }
         
         if (key == KeyEvent.VK_1) {
         	boolean taken = gamePanel.takeTransportRoute(1,playerX/speed, playerY/speed);
         	if (taken) {
-        		gamePanel.checkGemScore();
-        		gamePanel.checkCoinScore();
-        		gamePanel.popupIntersection();
-        		gamePanel.timer(50); 
+        		gamePanel.timer(50);
+                gamePanel.checkGemScore();
+                gamePanel.checkCoinScore();
+                gamePanel.popupIntersection();
+               if(gamePanel.allGemsCollected() == true) {
+                	gamePanel.restartGame();
+                };
         	}
         }
         
         if (key == KeyEvent.VK_2) {
         	boolean taken = gamePanel.takeTransportRoute(2,playerX/speed, playerY/speed);
         	if (taken) {
-        		gamePanel.checkGemScore();
-        		gamePanel.checkCoinScore();
-        		gamePanel.popupIntersection();
-        		gamePanel.timer(50); 
+        		gamePanel.timer(50);
+                gamePanel.checkGemScore();
+                gamePanel.checkCoinScore();
+                gamePanel.popupIntersection();
+               if(gamePanel.allGemsCollected() == true) {
+                	gamePanel.restartGame();
+                };
         	}
         }
     }
