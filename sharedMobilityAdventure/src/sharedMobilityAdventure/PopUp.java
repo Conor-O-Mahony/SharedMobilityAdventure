452c03af9ec21c617e5a1d2e0c7f810ee03f4be5
RiinKal <157484304+RiinKal@users.noreply.github.com>
2024-04-23 15:00:32 +0100

    Update PopUp.java
    Update PopUP class

--- sharedMobilityAdventure/src/sharedMobilityAdventure/PopUp.java
+++ sharedMobilityAdventure/src/sharedMobilityAdventure/PopUp.java
@@ -1,92 +1,105 @@
 package sharedMobilityAdventure;
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
-public class PopUp {
+public class PopUp implements Serializable{
+		
+	private static final long serialVersionUID = 2L;
+	protected int popupX;
+    protected int popupY;
+    private transient BufferedImage image;
+    int panelWidth = Main.DEFAULT_BOARD_SIZE;
+    int panelHeight = Main.DEFAULT_BOARD_SIZE;
+    protected static final int WIDTH = 32;
+    protected static final int HEIGHT = 32;
+    protected boolean visible;
     
-	public int popUpX; // x-coordinate
-	public int popUpY; // y-coordinate
-    final int width; // width of popUp
-    final int height; // height of popUp
-    BufferedImage image; // image of popUp
-    
-    //list of carbon emission related facts to choose from at random
+    private static transient Map<String, BufferedImage> popupCache = new HashMap<>();
+	
     private static String[] informationOptions = {
-        "Carbon emissions primarily consist of carbon dioxide (CO2)\n and other greenhouse gases released into the atmosphere.",
-        "The burning of fossil fuels like coal, oil, and natural gas\n is the largest source of carbon emissions.",
-        "The burning of coal for electricity generation is one of the\n largest sources of carbon emissions globally.",
-        "The United States, China, and the European Union are among the largest emitters of carbon dioxide globally.",
-        "Ireland has committed to achieving net-zero carbon emissions\n by 2050, in line with the goals of the Paris Agreement.",
-        "Ireland has a carbon tax in place to incentivize emission reductions\n and encourage the transition to cleaner energy sources.",
-        "The Irish government has established the Climate Action Plan, which outlines\n measures to achieve emissions reductions across various sectors.",
-        "The Irish forestry sector contributes to carbon sequestration by planting\n and managing forests, which absorb CO2 from the atmosphere.",
-        "The residential and commercial sectors contribute to carbon emissions\n through energy consumption for heating, cooling, and electricity use.",
-        "The transportation sector is one of the largest sources of carbon emissions\n in Ireland, accounting for approximately 20% of total emissions.",
-        "The Irish government offers incentives to promote the adoption of electric\n vehicles, such as grants for purchasing EVs and subsidies for installing charging infrastructure.",
-        "Ireland has implemented measures to improve the fuel efficiency of vehicles,\n such as emissions standards and mandatory vehicle inspections.",
-        "Ireland's National Transport Authority promotes sustainable transport options\n and invests in infrastructure to support walking, cycling, and public transit.",
-        "The Irish Rail network operates electric and diesel trains, with efforts underway\n to electrify more routes to further reduce carbon emissions.",
-        "Public transportation, including buses and trains, plays a significant role in reducing\n carbon emissions by providing an alternative to private car use.",
-        "Sustainable transportation initiatives, such as carpooling, ride-sharing, and telecommuting,\n help reduce carbon emissions by optimizing vehicle occupancy and reducing travel demand.",
-        "Ireland's Climate Action Plan includes measures to reduce carbon emissions from transportation,\n such as increasing the availability of electric vehicle charging stations and investing in public transit infrastructure.",
-        "The transportation of goods, including shipping and freight transport,\n contributes to carbon emissions through the burning of diesel fuel.",
-        "Carbon offsets can take various forms, including reforestation projects,\n renewable energy initiatives, and investments in energy efficiency.",
-        "The aviation industry is responsible for around 2% of global carbon emissions,\n and efforts are underway to develop sustainable aviation fuels and improve fuel efficiency."
+    "Carbon emissions primarily consist of carbon dioxide (CO2)\n and other greenhouse gases released into the atmosphere.",
+    "The burning of fossil fuels like coal, oil, and natural gas\n is the largest source of carbon emissions.",
+    "The burning of coal for electricity generation is one of the\n largest sources of carbon emissions globally.",
+    "The United States, China, and the European Union are among the largest emitters of carbon dioxide globally.",
+    "Ireland has committed to achieving net-zero carbon emissions\n by 2050, in line with the goals of the Paris Agreement.",
+    "Ireland has a carbon tax in place to incentivize emission reductions\n and encourage the transition to cleaner energy sources.",
+    "The Irish government has established the Climate Action Plan, which outlines\n measures to achieve emissions reductions across various sectors.",
+    "The Irish forestry sector contributes to carbon sequestration by planting\n and managing forests, which absorb CO2 from the atmosphere.",
+    "The residential and commercial sectors contribute to carbon emissions\n through energy consumption for heating, cooling, and electricity use.",
+    "The transportation sector is one of the largest sources of carbon emissions\n in Ireland, accounting for approximately 20% of total emissions.",
+    "The Irish government offers incentives to promote the adoption of electric\n vehicles, such as grants for purchasing EVs and subsidies for installing charging infrastructure.",
+    "Ireland has implemented measures to improve the fuel efficiency of vehicles,\n such as emissions standards and mandatory vehicle inspections.",
+    "Ireland's National Transport Authority promotes sustainable transport options\n and invests in infrastructure to support walking, cycling, and public transit.",
+    "The Irish Rail network operates electric and diesel trains, with efforts underway\n to electrify more routes to further reduce carbon emissions.",
+    "Public transportation, including buses and trains, plays a significant role in reducing\n carbon emissions by providing an alternative to private car use.",
+    "Sustainable transportation initiatives, such as carpooling, ride-sharing, and telecommuting,\n help reduce carbon emissions by optimizing vehicle occupancy and reducing travel demand.",
+    "Ireland's Climate Action Plan includes measures to reduce carbon emissions from transportation,\n such as increasing the availability of electric vehicle charging stations and investing in public transit infrastructure.",
+    "The transportation of goods, including shipping and freight transport,\n contributes to carbon emissions through the burning of diesel fuel.",
+    "Carbon offsets can take various forms, including reforestation projects,\n renewable energy initiatives, and investments in energy efficiency.",
+    "The aviation industry is responsible for around 2% of global carbon emissions,\n and efforts are underway to develop sustainable aviation fuels and improve fuel efficiency."
     };
-
-    public PopUp () {
-        width = 16;
-        height = 16;
-        try {
-            image = ImageIO.read(new File("images/tiles/water.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        dropPopUp();
-    }
-
-    public void dropPopUp() {
-            
-        Random random = new Random();
-        int randomNumberX = random.nextInt(16-1);
-        int randomNumberY = random.nextInt(8-1);
+ 
+    public PopUp() {
     	
+//    	System.out.println("Hello, world");
+    	
+    	this.visible = true;
+    	Random random = new Random();
+
+    	int randomNumberX = random.nextInt(panelWidth);
+        int randomNumberY = random.nextInt(panelHeight);
+
         int oddNumberX = randomNumberX * 2 + 1;
         int oddNumberY = randomNumberY * 2 + 1;
+
+        popupX = Main.TILE_SIZE / 2 * oddNumberX;
+        popupY = Main.TILE_SIZE / 2 * oddNumberY; 
         
-        popUpX = 8*4*oddNumberX;
-        popUpY = 8*4*oddNumberY;
-             
+        loadImage();
+            
+  }
+  
+    public boolean getVisibility() {
+        return this.visible;
     }
     
-    public void initialPopUp(String message) {
-        JOptionPane.showMessageDialog(null, message, "Initial Popup", JOptionPane.INFORMATION_MESSAGE);
+    public void loadImage() {
+    	if (popupCache.containsKey("info_popup")) {
+    		image = popupCache.get("info_popup");
+    	} else {
+    		try {
+				image = ImageIO.read(new File("images/info/info_popup.png"));
+				popupCache.put("info_popup", image);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        }
     }
-    
-    public void informationPopUp() {
-        // Generate a random index to pick a random message from the list
-        int randomIndex = new Random().nextInt(informationOptions.length);
-        
-        // Show a JOptionPane with the randomly picked message
-        JOptionPane.showMessageDialog(null,
-                informationOptions[randomIndex],
-                "Carbon Fact! Did You Know?",
-                JOptionPane.INFORMATION_MESSAGE);
-    }
-   
-    public void draw(Graphics g) {
-        int adjustedX = popUpX - (width / 2);
-        int adjustedY = popUpY - (height / 2);
 
-        g.drawImage(image, adjustedX, adjustedY, width, height, null);
-    }
-         	
+  public void draw(Graphics g) {
+      int adjustedX = popupX - (WIDTH / 2);
+      int adjustedY = popupY - (HEIGHT / 2);
+      g.drawImage(image, adjustedX, adjustedY, WIDTH, HEIGHT, null);
+  }
 
+public void setVisibility(boolean visibleUpdated) {
+    this.visible = visibleUpdated;
+	
+}
 
+public void displayPopup() {
+	
+    Random randomObj = new Random();
+    int randomNumber = randomObj.nextInt(20);
+	JOptionPane.showMessageDialog(null, "Did you know: " + informationOptions[randomNumber]);	
+} 
 }
